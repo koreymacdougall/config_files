7a508816ed1ce5d52a98726677927f1fb37dadf3
koreymacdougall <koreymacdougall@gmail.com>
2017-01-02 15:02:04 -0500

    added terminator settings, and starting to script symlinking

---
+++ mk_sym_links.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm ~/.config/terminator/config
+ln -s ~/config_files/.terminator_config ~/.config/terminator/config
+ln -s ~/config_files/.vimrc ~/.vimrc
+#ln -s ~/config_files/.vimrc ~/.vimrc
+#for xmodmap and xinitrc -- ln -s ~/config_files/.vimrc ~/.vimrc
