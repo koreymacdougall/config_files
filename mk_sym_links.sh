ea0bf856c9e30e3ba208f1a4cdd57a9c7fc41dc2
koreymacdougall <koreymacdougall@gmail.com>
2017-01-03 14:11:21 -0500

    fixed elinks conf

--- mk_sym_links.sh
+++ mk_sym_links.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 rm ~/.config/terminator/config
 ln -s ~/config_files/.terminator_config ~/.config/terminator/config
+
 rm ~/.vimrc
 ln -s ~/config_files/.vimrc ~/.vimrc
-#make symlinks for xmledit to work on both html and erb files
-ln -s ~/.vim/plugged/xmledit/ftplugin/xml.vim ~/.vim/plugged/xmledit/ftplugin/html.vim
+
 rm ~/.elinks/elinks.conf
 ln -s ~/config_files/.elinks_conf ~/.elinks/elinks.conf
+
+#make symlinks for xmledit to work on both html and erb files
+rm ~/.vim/plugged/xmledit/ftplugin/html.vim
+ln -s ~/.vim/plugged/xmledit/ftplugin/xml.vim ~/.vim/plugged/xmledit/ftplugin/html.vim
+
 #for xmodmap and xinitrc...run manually? 
