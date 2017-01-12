220adfa0c44079aabea59f3f625ca6a35537a478
koreymacdougall <korey.macdougall@gmail.com>
2017-01-12 12:46:45 -0500

    added arch-specific keymap file & symlink

--- mk_sym_links.sh
+++ mk_sym_links.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-#rm ~/.config/terminator/config
 ln -sf ~/config_files/.terminator_config ~/.config/terminator/config
 
 #rm ~/.vimrc
 ln -sf ~/config_files/.vimrc ~/.vimrc
 
-#rm ~/.elinks/elinks.conf
+#first is for mint/lubuntu, second is for arch
 ln -sf ~/config_files/.elinks_conf ~/.elinks/elinks.conf
+ln -sf ~/config_files/.elinks_conf ~/.elinks.conf
 
-#rm ~/.vim/.skeleton.html
 ln -sf  ~/config_files/.vim_snippets/.skeleton.html ~/.vim/.skeleton.html
 
 #make symlinks for xmledit to work on both html and erb files
-#rm ~/.vim/plugged/xmledit/ftplugin/html.vim
 ln -sf ~/.vim/plugged/xmledit/ftplugin/xml.vim ~/.vim/plugged/xmledit/ftplugin/html.vim
 
-#for xmodmap and xinitrc...run manually? 
-
-
+#this currently works on arch... I don't know about bunt/deb derivs
+ln -sf ~/config_files/.us.map.custom.gz /usr/share/kbd/keymaps/i386/qwerty
+sudo localectl set-keymap --no-convert us.map.custom
